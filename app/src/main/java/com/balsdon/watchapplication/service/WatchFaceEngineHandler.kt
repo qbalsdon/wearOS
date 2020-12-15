8f2928a7e41030ee35ac5f37efa2131542e6ef3a
Quintin <quintin.balsdon@zuhlke.com>
2020-12-15 22:01:11 +0000

    added comment preventing myself from further refactoring

--- app/src/main/java/com/balsdon/watchapplication/service/WatchFaceEngineHandler.kt
+++ app/src/main/java/com/balsdon/watchapplication/service/WatchFaceEngineHandler.kt
@@ -11,7 +11,12 @@ import android.view.SurfaceHolder
  * a neater structure can be made.
  *
  * The Engine handler is an interface that allows the Watch Face to update different properties
- * and demonstrates the need for simplicity within the Wear OS Framework.
+ * and demonstrates the need for simplicity within the Wear OS Framework. The [WatchScreenSettings]
+ * class demonstrates that the collection of the display proprties should be grouped together
+ * rather than relying on [WatchFaceEngine.onPropertiesChanged] and
+ * [WatchFaceEngine.onAmbientModeChanged] distinctly. However it must be noted that Ambient mode is
+ * new to the Wathc API and Watch Faces are a hack on Android WallPapers. That's why they're
+ * distinct.
  */
 interface WatchFaceEngineHandler {
     fun engineCreated()
